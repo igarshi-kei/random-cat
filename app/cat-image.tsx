4ce1566cdda52d634f5a4859239508b55fe33ea0
kei_315023 <igarashi.kei.ku@tut.jp>
2025-10-14 11:27:50 +0900

    猫画像ジェネレーター完成

---
+++ app/cat-image.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+import Styles from "./page.module.css";
+
+type CatImageProps = {
+    url: string;
+};
+
+export function CatImage({ url }: CatImageProps) {
+    const [imageUrl, setImageUrl] = useState(url);
+
+    const refreshImage = async () => {
+        setImageUrl(""); // 画像を一旦クリアしてローディング状態を表現
+        const image = await fetchImage();
+        setImageUrl(image.url);
+    }
+
+    return (
+        <div className={Styles.page}>
+            <button onClick={refreshImage} className={Styles.button}>
+                他のにゃんこも見る
+            </button>
+            <div className={Styles.frame}>
+                {imageUrl && <img src={imageUrl} className={Styles.img} />}
+            </div>
+        </div>
+    );
+}
